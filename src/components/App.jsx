e98c6a3204154c26ea45ada7b396768208d05775
vlad-lab319 <solo15mag@gmail.com>
2022-05-07 10:47:23 -0400

    Initial commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 import './App.css';
 import useApplicationData from '../hooks/useApplicationData';
 import ProductsList from './ProductsList';
+import Navbar from './Navbar';
 
 function App() {
 
@@ -9,23 +10,36 @@ function App() {
   //   {fulhausProductName: "Product 1"},
   //   {fulhausProductName: "Product 2"},
   // ]
-  
-  // Fetched data
-  const productsFetched = useApplicationData();
+
+  // App data
+  const {
+    products, 
+    cart, 
+    addToCart,
+    removeFromCart
+  } = useApplicationData();
 
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          React starts from here
-        </p>
-        <section>
-          <ProductsList
-            // products={products}
-            productsF={productsFetched.products}
-          />
-        </section>
-      </header>
+      <Navbar 
+      cart={cart}
+      removeFromCart={removeFromCart}
+      />
+
+      <div className="main-page">
+        <img
+          className="main-img"
+          src="https://cdn.sanity.io/images/swpm962j/production/6e6ad658018ae5d35751820b11f09159709f7211-2656x1228.jpg" alt="Nuevo-heroImage"
+        />
+      </div>
+
+      <section>
+        <ProductsList
+          products={products}
+          addToCart={addToCart}
+        />
+      </section>
+
     </div>
   );
 }
